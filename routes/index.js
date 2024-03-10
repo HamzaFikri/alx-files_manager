e72fabcb35fbdb901018a6d0e5af0b7fa5f0014d
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2024-03-10 17:29:50 +0000

    es-lint - Add authentication endpoints and middleware

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
-import { basicAuthenticate, xTokenAuthenticate } from '../components/auth';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 
 const router = Router();
 
