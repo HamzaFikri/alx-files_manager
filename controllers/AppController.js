4960a81de28ca4c9b704f899a1a36ab311134102
HamzaFikri <mohammed.hamza.fikri@gmail.com>
2024-03-11 17:17:39 +0000

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,20 +3,16 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static async getStatus(req, res) {
-    const redisStatus = redisClient.isAlive();
-    const dbStatus = dbClient.isAlive();
-    res.status(200).json({
-      redis: redisStatus,
-      db: dbStatus,
-    });
+  static getStatus(req, res) {
+    const redisLive = redisClient.isAlive();
+    const dbLive = dbClient.isAlive();
+    res.status(200).json({ redis: redisLive, db: dbLive });
   }
 
   static async getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
+    const usersTotal = await dbClient.nbUsers();
+    const filesTotal = await dbClient.nbFiles();
+    res.status(200).json({ users: usersTotal, files: filesTotal });
   }
 }
 
